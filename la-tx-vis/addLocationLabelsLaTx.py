40b2f9ed4cae59cd8869184c8cc259bac88e4872
Marcos Botto Tornielli <m.botto_t@utexas.edu>
2020-08-04 15:54:29 -0500

    Add latx location labels script

---
+++ la-tx-vis/addLocationLabelsLaTx.py
@@ -0,0 +1,55 @@
+#### import the simple module from the paraview
+from paraview.simple import *
+#### disable automatic camera reset on 'Show'
+paraview.simple._DisableFirstRenderCameraReset()
+
+text1 = Text()
+text1Display = Show(text1, renderView1, 'TextSourceRepresentation')
+text1.Text = 'Houston .'
+text1Display.FontSize = 9
+text1Display.Bold = 1
+text1Display.Shadow = 1
+text1Display.WindowLocation = 'AnyLocation'
+text1Display.Position = [0.065, 0.74]
+renderView1.Update()
+
+text2 = Text()
+text2Display = Show(text2, renderView1, 'TextSourceRepresentation')
+text2.Text = 'Galveston .'
+text2Display.FontSize = 9
+text2Display.Bold = 1
+text2Display.Shadow = 1
+text2Display.WindowLocation = 'AnyLocation'
+text2Display.Position = [0.1, 0.64]
+renderView1.Update()
+
+text3 = Text()
+text3Display = Show(text3, renderView1, 'TextSourceRepresentation')
+text3.Text = '. Bolivar Peninsula'
+text3Display.FontSize = 6
+text3Display.Bold = 1
+text3Display.Shadow = 1
+text3Display.WindowLocation = 'AnyLocation'
+text3Display.Position = [0.22, 0.66]
+renderView1.Update()
+
+text4 = Text()
+text4Display = Show(text4, renderView1, 'TextSourceRepresentation')
+text4.Text = '. New Orleans'
+text4Display.FontSize = 9
+text4Display.Bold = 1
+text4Display.Shadow = 1
+text4Display.WindowLocation = 'AnyLocation'
+text4Display.Position = [0.72, 0.77]
+renderView1.Update()
+
+text5 = Text()
+text5Display = Show(text5, renderView1, 'TextSourceRepresentation')
+text5.Text = '. Lake Pontchartrain'
+text5Display.FontSize = 6
+text5Display.Bold = 1
+text5Display.Shadow = 1
+text5Display.WindowLocation = 'AnyLocation'
+text5Display.Position = [0.71, 0.82]
+renderView1.Update()
+
