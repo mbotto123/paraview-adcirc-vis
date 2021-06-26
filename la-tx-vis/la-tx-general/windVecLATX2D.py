c12a3213fb8e060cf55f10bbe683552c09774d5a
mbotto123 <m.botto_t@utexas.edu>
2021-06-26 18:39:46 -0500

    Make wind vectors 3D and filter out vectors on dry nodes

--- la-tx-vis/la-tx-general/windVecLATX2D.py
+++ la-tx-vis/la-tx-general/windVecLATX2D.py
@@ -17,21 +17,23 @@ surfaceVectors1.SelectInputVectors = ['POINTS', 'windVel']
 surfaceVectors1Display = Show(surfaceVectors1, renderView1, 'UnstructuredGridRepresentation')
 
 # create a new 'Glyph'
-glyph1 = Glyph(Input=surfaceVectors1, GlyphType='2D Glyph')
+glyph1 = Glyph(Input=surfaceVectors1, GlyphType='Arrow')
 glyph1.OrientationArray = ['POINTS', 'windVel']
 glyph1.GlyphTransform = 'Transform2'
 glyph1.ScaleArray = ['POINTS', 'windVel']
 # This scale factor is intended for hurricane-level winds; it will be too small for normal winds
 glyph1.ScaleFactor = 0.01
 glyph1.MaximumNumberOfSamplePoints = 10000
-
-glyph1Display = Show(glyph1, renderView1, 'GeometryRepresentation')
-
-# turn off scalar coloring
-ColorBy(glyph1Display, None)
-
-# Properties modified on glyph1
+glyph1.GlyphType.ShaftRadius = 0.02
 glyph1.GlyphMode = 'Uniform Spatial Distribution (Surface Sampling)'
+
+# Filter out wind vectors on dry nodes
+threshold2 = Threshold(Input=glyph1)
+threshold2.Scalars = ['POINTS', 'zeta']
+threshold2.ThresholdRange = [-9999.0, 7877.455]
+
+threshold2Display = Show(threshold2, renderView1, 'UnstructuredGridRepresentation')
+ColorBy(threshold2Display, None)
 
 # update the view to ensure updated data information
 renderView1.Update()
