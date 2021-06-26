043d59e09fb8f88d7002d65b5bdf32705bb816c3
mbotto123 <m.botto_t@utexas.edu>
2021-06-26 18:17:34 -0500

    major update - labels are now 3D billboard text

--- la-tx-vis/la-tx-general/labelsLATX2D-ML.py
+++ la-tx-vis/la-tx-general/labelsLATX2D-ML.py
@@ -7,75 +7,117 @@ paraview.simple._DisableFirstRenderCameraReset()
 
 renderView1 = GetActiveViewOrCreate('RenderView')
 
+## Houston
 text1 = Text()
 text1Display = Show(text1, renderView1, 'TextSourceRepresentation')
-text1.Text = 'Houston .'
-text1Display.FontSize = 9
+text1Display.TextPropMode = 'Billboard 3D Text'
+text1.Text = 'Houston '
+text1Display.FontSize = 30
 text1Display.Bold = 1
 text1Display.Shadow = 1
-text1Display.WindowLocation = 'AnyLocation'
-text1Display.Position = [0.065, 0.74]
-renderView1.Update()
+text1Display.Justification = 'Right'
+text1Display.BillboardPosition = [-95.3698, 29.7604, 0.05]
 
+sphere1 = Sphere()
+sphere1.Center = [-95.3698, 29.7604, 0.0]
+sphere1.Radius = 0.025
+sphere1Display = Show(sphere1, renderView1, 'GeometryRepresentation')
+
+## Galveston
 text2 = Text()
 text2Display = Show(text2, renderView1, 'TextSourceRepresentation')
-text2.Text = 'Galveston .'
-text2Display.FontSize = 9
+text2Display.TextPropMode = 'Billboard 3D Text'
+text2.Text = 'Galveston '
+text2Display.FontSize = 30
 text2Display.Bold = 1
 text2Display.Shadow = 1
-text2Display.WindowLocation = 'AnyLocation'
-text2Display.Position = [0.1, 0.64]
-renderView1.Update()
+text2Display.Justification = 'Right'
+text2Display.BillboardPosition = [-94.7977, 29.3013, 0.05]
 
+sphere2 = Sphere()
+sphere2.Center = [-94.7977, 29.3013, 0.0]
+sphere2.Radius = 0.025
+sphere2Display = Show(sphere2, renderView1, 'GeometryRepresentation')
+
+## Bolivar Peninsula
 text3 = Text()
 text3Display = Show(text3, renderView1, 'TextSourceRepresentation')
-text3.Text = '. Bolivar Peninsula'
-text3Display.FontSize = 6
+text3Display.TextPropMode = 'Billboard 3D Text'
+text3.Text = ' Bolivar Peninsula'
+text3Display.FontSize = 25
 text3Display.Bold = 1
 text3Display.Shadow = 1
-text3Display.WindowLocation = 'AnyLocation'
-text3Display.Position = [0.22, 0.66]
-renderView1.Update()
+text3Display.Justification = 'Left'
+text3Display.BillboardPosition = [-94.5799, 29.4783, 0.05]
 
+sphere3 = Sphere()
+sphere3.Center = [-94.5799, 29.4783, 0.0]
+sphere3.Radius = 0.025
+sphere3Display = Show(sphere3, renderView1, 'GeometryRepresentation')
+
+## New Orleans
 text4 = Text()
 text4Display = Show(text4, renderView1, 'TextSourceRepresentation')
-text4.Text = '. New Orleans'
-text4Display.FontSize = 9
+text4Display.TextPropMode = 'Billboard 3D Text'
+text4.Text = ' New Orleans'
+text4Display.FontSize = 30
 text4Display.Bold = 1
 text4Display.Shadow = 1
-text4Display.WindowLocation = 'AnyLocation'
-text4Display.Position = [0.72, 0.77]
-renderView1.Update()
+text4Display.Justification = 'Left'
+text4Display.BillboardPosition = [-90.0715, 29.9511, 0.05]
 
+sphere4 = Sphere()
+sphere4.Center = [-90.0715, 29.9511, 0.0]
+sphere4.Radius = 0.025
+sphere4Display = Show(sphere4, renderView1, 'GeometryRepresentation')
+
+## Lake Pontchartrain
 text5 = Text()
 text5Display = Show(text5, renderView1, 'TextSourceRepresentation')
-text5.Text = '. Lake Pontchartrain'
-text5Display.FontSize = 6
+text5Display.TextPropMode = 'Billboard 3D Text'
+text5.Text = ' Lake Pontchartrain'
+text5Display.FontSize = 25
 text5Display.Bold = 1
 text5Display.Shadow = 1
-text5Display.WindowLocation = 'AnyLocation'
-text5Display.Position = [0.71, 0.82]
-renderView1.Update()
+text5Display.Justification = 'Left'
+text5Display.BillboardPosition = [-90.1121, 30.2051, 0.05]
 
+sphere5 = Sphere()
+sphere5.Center = [-90.1121, 30.2051, 0.0]
+sphere5.Radius = 0.025
+sphere5Display = Show(sphere5, renderView1, 'GeometryRepresentation')
+
+## Lake Charles
 text6 = Text()
 text6Display = Show(text6, renderView1, 'TextSourceRepresentation')
-text6.Text = '. Lake Charles'
-text6Display.FontSize = 9
+text6Display.TextPropMode = 'Billboard 3D Text'
+text6.Text = ' Lake Charles'
+text6Display.FontSize = 30
 text6Display.Bold = 1
 text6Display.Shadow = 1
-text6Display.WindowLocation = 'AnyLocation'
-text6Display.Position = [0.38, 0.83]
-renderView1.Update()
+text6Display.Justification = 'Left'
+text6Display.BillboardPosition = [-93.2174, 30.2266, 0.0]
 
+sphere6 = Sphere()
+sphere6.Center = [-93.2174, 30.2266, 0.0]
+sphere6.Radius = 0.025
+sphere6Display = Show(sphere6, renderView1, 'GeometryRepresentation')
+
+## Port Arthur
 text7 = Text()
 text7Display = Show(text7, renderView1, 'TextSourceRepresentation')
-text7.Text = 'Port Arthur .'
-text7Display.FontSize = 8
+text7Display.TextPropMode = 'Billboard 3D Text'
+text7.Text = 'Port Arthur '
+text7Display.FontSize = 28
 text7Display.Bold = 1
 text7Display.Shadow = 1
-text7Display.WindowLocation = 'AnyLocation'
-text7Display.Position = [0.21, 0.75]
+text7Display.Justification = 'Right'
+text7Display.BillboardPosition = [-93.9399, 29.8850, 0.0]
+
+sphere7 = Sphere()
+sphere7.Center = [-93.9399, 29.8850, 0.0]
+sphere7.Radius = 0.025
+sphere7Display = Show(sphere7, renderView1, 'GeometryRepresentation')
+
+## Update render view with all changes at once
 renderView1.Update()
-
-
-
