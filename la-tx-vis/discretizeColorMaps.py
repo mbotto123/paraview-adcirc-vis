33b7214861e938b52a5adc9cc5617d285cdff6da
mbotto123 <m.botto_t@utexas.edu>
2021-06-26 19:51:35 -0500

    Add utility script to discretize color maps

---
+++ la-tx-vis/discretizeColorMaps.py
@@ -0,0 +1,20 @@
+# Script to discretize color maps
+# Compatible with any ADCIRC output file that contains bathmetry/topography and water surface elevation 
+
+#### import the simple module from the paraview
+from paraview.simple import *
+#### disable automatic camera reset on 'Show'
+paraview.simple._DisableFirstRenderCameraReset()
+
+# get color transfer function/color map for 'zeta'
+if (FindSource('maxele.63.nc.xmf')) is not None:
+	zetaLUT = GetColorTransferFunction('zeta_max')
+else:
+	zetaLUT = GetColorTransferFunction('zeta')
+
+# get color transfer function/color map for 'BathymetricDepth'
+bathymetricDepthLUT = GetColorTransferFunction('BathymetricDepth')
+
+# Discretize color maps by reducing the number of table values
+zetaLUT.NumberOfTableValues = 50
+bathymetricDepthLUT.NumberOfTableValues = 50
